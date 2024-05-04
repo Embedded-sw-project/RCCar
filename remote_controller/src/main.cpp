eaf8c7e98858e082e11fc0997a93d1a7c4632d6f
MattBrs <matteobrusarosco@tutamail.com>
2024-05-04 23:35:47 +0200

    [transmitter] write function. setup as transmitter instead of receiver.

--- remote_controller/src/main.cpp
+++ remote_controller/src/main.cpp
@@ -9,15 +9,42 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 
-RF24 radio(25, 28);
+#define CE_PIN 25
+#define CSN_PIN 28
+
+RF24 radio(CE_PIN, CSN_PIN);  
+const uint8_t address[][6] = {"1Node"};
+const byte sampleText[6] = "hello";
+
+void transmitData(const byte data[]);
 
 // Add setup code
 void setup() {
     Serial.begin(9600); // for LCD debug output
+
+    radio.begin();    
+
+    // opens a pipe for writing byte arrays
+    radio.openWritingPipe(address[0]);
+
+    // power level for range. Keep at MIN to avoid issues
+    // high power levels give too much power to near antennas
+    // and communications do not finish successfully
+    radio.setPALevel(RF24_PA_MIN); 
+    
+    /* this should be used if we want to receive and not transmit */
+    // radio.startListening(); 
+    
 }
 
 // Add loop code
 void loop()
 {
+    transmitData(sampleText);
+}
 
+
+void transmitData(const byte* data) {
+    radio.stopListening(); // always enforce closure to be sure
+    radio.write(&data, sizeof(data));
 }
