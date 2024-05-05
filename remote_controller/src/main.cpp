ff9d578487eec0f848371e89f854167a116b512a
MattBrs <matteobrusarosco@tutamail.com>
2024-05-05 12:00:26 +0200

    [serial] Setup serial communication. Move init to functions.

--- remote_controller/src/main.cpp
+++ remote_controller/src/main.cpp
@@ -1,4 +1,7 @@
 // Core library for code-sense
+#include "msp432p401r.h"
+#include "rom_map.h"
+#include "uart.h"
 #if defined(ENERGIA) // LaunchPad MSP430, Stellaris and Tiva, Experimeter Board
                      // FR5739 specific
 #include "Energia.h"
@@ -6,7 +9,11 @@
 #error Platform not defined
 #endif
 
+#include "printf.h"
+#include "stdbool.h"
+#include "stdint.h"
 #include <RF24.h>
+#include <driverlib.h>
 #include <nRF24L01.h>
 
 #define CE_PIN 25
@@ -16,12 +23,36 @@ RF24          radio(CE_PIN, CSN_PIN);
 const uint8_t address[][6] = {"1Node"};
 const byte    sampleText[6] = "hello";
 
+
+/* UART Configuration Parameter. These are the configuration parameters to
+ * make the eUSCI A UART module to operate with a 2400 baud rate. These
+ * values were calculated using the online calculator that TI provides
+ * at:
+ *http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
+ */
+const eUSCI_UART_Config uartConfig = {
+    EUSCI_A_UART_CLOCKSOURCE_SMCLK,               // SMCLK Clock Source
+    78,                                           // BRDIV = 78
+    2,                                            // UCxBRF = 2
+    0,                                            // UCxBRS = 0
+    EUSCI_A_UART_NO_PARITY,                       // No Parity
+    EUSCI_A_UART_LSB_FIRST,                       // MSB First
+    EUSCI_A_UART_ONE_STOP_BIT,                    // One stop bit
+    EUSCI_A_UART_MODE,                            // UART mode
+    EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION // Oversampling
+};
+
+void setupUART();
+void setupRF24();
 void transmitData(const byte data[]);
 
 // Add setup code
 void setup() {
-    Serial.begin(9600); // for LCD debug output
+    setupUART();
+    setupRF24();
+}
 
+void setupRF24() {
     radio.begin();
 
     // opens a pipe for writing byte arrays
@@ -31,13 +62,36 @@ void setup() {
     // high power levels give too much power to near antennas
     // and communications do not finish successfully
     radio.setPALevel(RF24_PA_MIN);
-    
+
     /* this should be used if we want to receive and not transmit */
     // radio.startListening();
 }
 
+void setupUART() {
+    /* Halting WDT  */
+    MAP_WDT_A_holdTimer();
+
+    /* Selecting P1.2 and P1.3 in UART mode */
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(
+        GPIO_PORT_P1, GPIO_PIN1 | GPIO_PIN2 | GPIO_PIN3,
+        GPIO_PRIMARY_MODULE_FUNCTION);
+
+    /* Setting DCO to 12MHz */
+    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
+
+    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
+    MAP_UART_enableModule(EUSCI_A0_BASE);
+
+    UART_initModule(EUSCI_A0_BASE, &uartConfig);
+    UART_enableModule(EUSCI_A0_BASE);
+}
+
 // Add loop code
 void loop() {
+    char hello[7] = "hello\n";
+    printf(EUSCI_A0_BASE, hello);
+    bool res = radio.isChipConnected();
+
     transmitData(sampleText);
 }
 
